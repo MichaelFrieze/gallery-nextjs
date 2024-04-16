573efc96e27cf138118fc570210118f9002e7003
Michael Frieze <michaelfrieze@icloud.com>
2024-04-15 21:11:24 -0400

    display mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,21 @@ const mockUrls = [
   "https://utfs.io/f/bb371ca9-e785-497b-b354-6b3636870851-bt5zuj.jpg",
 ];
 
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
