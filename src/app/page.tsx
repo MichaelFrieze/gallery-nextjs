3a3529517e04d9eeb348fde2e409e8fb0d315291
Michael Frieze <michaelfrieze@icloud.com>
2024-04-15 21:03:18 -0400

    add mock URLs and update gitignore to ignore video timestamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/dbf9f471-fd12-46c5-80b4-aaa81b7c4f05-u21jad.webp",
+  "https://utfs.io/f/79e6c6a2-397b-4124-a15d-c49fecf937ff-vuy015.webp",
+  "https://utfs.io/f/299dcaa9-4e1d-4e88-8912-63f2a0482299-j99ia1.jpg",
+  "https://utfs.io/f/bb371ca9-e785-497b-b354-6b3636870851-bt5zuj.jpg",
+];
+
 export default function HomePage() {
   return (
     <div>
