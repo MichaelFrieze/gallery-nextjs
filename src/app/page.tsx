3759be1e55ede92d8db3f41326bae35b3a792e7e
Michael Frieze <michaelfrieze@icloud.com>
2024-04-17 20:04:11 -0400

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/dbf9f471-fd12-46c5-80b4-aaa81b7c4f05-u21jad.webp",
@@ -6,18 +7,24 @@ const mockUrls = [
   "https://utfs.io/f/299dcaa9-4e1d-4e88-8912-63f2a0482299-j99ia1.jpg",
   "https://utfs.io/f/bb371ca9-e785-497b-b354-6b3636870851-bt5zuj.jpg",
 ];
-
 const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
