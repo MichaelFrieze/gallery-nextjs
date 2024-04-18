c11f9759bf46952aa2ca546d6e2233b377c76fe4
Michael Frieze <michaelfrieze@icloud.com>
2024-04-18 10:09:34 -0400

    link the database to the UI

--- README.md
+++ README.md
@@ -4,16 +4,13 @@
 
 - [x] Make it deploy (vercel)
 - [x] Scaffold basic ui with mock data
-- [] Tidy up build process
-- [] Actually set up a database (vercel postgres)
-- [] Attach database to UI
-- [] Add authentication (w/ clerk)
-- [] Add image upload
-- [] "taint" (server-only)
-- [] Use Next/Image component
-- [] Error management (w/ Sentry)
-- [] Routing/image page (parallel route)
-- [] Update upload button to be less cringe
-- [] Analytics (posthog)
-- [] Delete button (w/ Server Actions)
-- [] Ratelimiting (upstash)
+- [x] Tidy up build process
+- [x] Actually set up a database (vercel postgres)
+- [x] Attach database to UI
+- [ ] Add authentication (w/ clerk)
+- [ ] Add image upload
+- [ ] Error management (w/ Sentry)
+- [ ] Routing/image page (parallel route)
+- [ ] Delete button (w/ Server Actions)
+- [ ] Analytics (posthog)
+- [ ] Ratelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,18 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/dbf9f471-fd12-46c5-80b4-aaa81b7c4f05-u21jad.webp",
-  "https://utfs.io/f/79e6c6a2-397b-4124-a15d-c49fecf937ff-vuy015.webp",
-  "https://utfs.io/f/299dcaa9-4e1d-4e88-8912-63f2a0482299-j99ia1.jpg",
-  "https://utfs.io/f/bb371ca9-e785-497b-b354-6b3636870851-bt5zuj.jpg",
-];
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
